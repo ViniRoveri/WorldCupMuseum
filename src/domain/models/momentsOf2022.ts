0cc99407affabd33503e9555989d2d276f45eb17
Vinícius Roveri <viniciusroverirocha@gmail.com>
2025-04-01 15:08:41 -0300

    01/04/2025

--- src/domain/models/momentsOf2022.ts
+++ src/domain/models/momentsOf2022.ts
@@ -2,13 +2,15 @@ import BreakingTheScriptIn2Minutes from "@/components/moments/2022/ArgentinaFran
 import Year from "../types/Year";
 import TheGreatestSaveOfAllTime from "@/components/moments/2022/ArgentinaFrance/TheGreatestSaveOfAllTime";
 import WhenThePrinceAlmostBecameAKing from "@/components/moments/2022/CroatiaBrasil/WhenThePrinceAlmostBecameAKing";
+import TheArgentineSpiderMan from "@/components/moments/2022/ArgentinaCroatia/TheArgentineSpiderMan";
+import AfricasGreatestWhatIf from "@/components/moments/2022/FranceMorocco/AfricasGreatestWhatIf";
 
 const momentsOf2022: Year = {
    year: 2022,
    country: 'Qatar',
    rounds: [
       {
-         title: 'Quarter-finals',
+         title: 'Quarter-Finals',
          games: [
             {
                title: 'Croatia 1 (4) x (2) 1 Brazil',
@@ -19,6 +21,35 @@ const momentsOf2022: Year = {
                      minutes: "105+1'",
                      component: WhenThePrinceAlmostBecameAKing,
                      text: `Following a tense and uneventful 0x0 draw in the 90 minutes, the game heads to extra time. After the first 15 minutes, Neymar starts a great passing play that ends up leading the ball right back to him face to face with Croatia's goalkeeper. At that situation Neymar dribbles the goalkeeper with a single touch and scores what is one of the most anthological goals of his whole career.`
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         title: 'Semi-Finals',
+         games: [
+            {
+               title: 'Argentina 3 x 0 Croatia',
+               moments: [
+                  {
+                     id: 'TheArgentineSpiderMan',
+                     title: 'The Argentine Spider-Man',
+                     minutes: "38'",
+                     component: TheArgentineSpiderMan,
+                     text: `Argentina is already leading 1x0 near the end of the first half in a fairly balanced game, that is until Julián Alvarez decides to take the ball on the defending side of the field and take it with himself going through the whole croatian defence and finishing up with an acrobatic jump to kick the ball into the net and extend Argentina's lead.`
+                  }
+               ]
+            },
+            {
+               title: 'France 2 x 0 Morocco',
+               moments: [
+                  {
+                     id: 'AfricasGreatestWhatIf',
+                     title: `Africa's Greatest "What If"`,
+                     minutes: "43'",
+                     component: AfricasGreatestWhatIf,
+                     text: `Starting the game Morocco already had the achievement of being the first african team in history to reach the semi-finals of the World Cup, and looking forward to making even more history by reaching the grand finals. Nearing the end of the first half they are 1x0 down against France, but after a corner kick the ball goes flying in the box and Morocco's defender Jawad El Yamiq pulls out a bicycle kick that surprise everyone and that forces France's goalkeeper Lloris to make an extraordinary save to keep France ahead and take out Morocco's best chance to tie the game so far.`
                   }
                ]
             }
